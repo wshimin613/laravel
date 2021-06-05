88ab32f16c7145a2c21d71b6eb2d5709993fb89c
min <ming890613@gmail.com>
2021-06-05 19:11:46 +0800

    刪除資源以及異常處理

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,8 +5,42 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Traits\ApiResponseTrait;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 class Handler extends ExceptionHandler
 {
+    use ApiResponseTrait;
+
+    public function render($request, Throwable $exception)
+    {
+        if($request->expectsJson()){
+            if ($exception instanceof ModelNotFoundException) {
+                return $this->errorResponse(
+                    '找不到資源',
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+            if ($exception instanceof NotFoundHttpException) {
+                return $this->errorResponse(
+                    '無法找到此網址',
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+            if ($exception instanceof MethodNotAllowedHttpException) {
+                return $this->errorResponse(
+                    $exception->getMessage(),
+                    Response::HTTP_METHOD_NOT_ALLOWED
+                );
+            }
+        }
+        return parent::render($request, $exception);
+    }
+
     /**
      * A list of the exception types that are not reported.
      *
---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponseTrait
+{
+    public function errorResponse($message, $status, $code=null)
+    {
+        $code = $code ?? $status;
+
+        return response()->json(
+            [
+                'message' => $message,
+                'code' => $code
+            ],
+            $status
+        );
+    }
+}
